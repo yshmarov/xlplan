3e75afe3fe36f620fcd4fd46aa34c0c78a596e83
yashm <yshmarov@gmail.com>
2018-11-05 23:04:34 +0000

    create calendar view

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!, :only => [ :landing_page ]
-  def landing_page
+  def landing_page; end
+
+  def calendar
+    @events = Event.all
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  resources :events
-  resources :clients
-  resources :employee_categories
-  resources :employees
   devise_for :users, controllers: { invitations: 'invitations'}
   #devise_for :users, controllers: { registrations: "registrations", invitations: 'invitations'}
   #devise_for :users
@@ -12,9 +8,15 @@ Rails.application.routes.draw do
   		patch :invite_user
     end
   end
+  resources :clients
+  resources :employees
+
   resources :categories
-  resources :machines
-  resources :locations
-  root to: 'static_pages#landing_page'
   resources :services
+  resources :employee_categories
+  resources :locations
+  resources :machines
+  resources :events
+  get 'calendar', to: 'static_pages#calendar'
+  root to: 'static_pages#landing_page'
 end
