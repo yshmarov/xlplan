1007a16129fd76268c77e2d2efa7f1e6623465a5
yashm <yshmarov@gmail.com>
2018-11-10 23:03:17 +0000

    employee validation: start<end

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -5,17 +5,24 @@ class Employee < ApplicationRecord
 
   validates :person_id, :location_id, :status, presence: true
   validates :person_id, uniqueness: true
-
+  validate :termination_date_cannot_be_before_employment_date
+  
   monetize :balance, as: :balance_cents
   after_touch :update_balance
 
   #scope :active, -> { where(status: 0) }
 
+  enum status: [:"Active", :"Inactive"]
+
   def to_s
     person
   end
 
-  enum status: [:"Active", :"Inactive"]
+  def termination_date_cannot_be_before_employment_date
+    if termination_date.present? && termination_date <= employment_date
+      errors.add(:termination_date, "can't be before employment date")
+    end
+  end
 
   protected
   
