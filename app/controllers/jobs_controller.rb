e1234c19c5c20f70d29ee812581d74cba69112da
yashm <yshmarov@gmail.com>
2018-11-11 23:13:27 +0000

    undo commentable jobs (not needed now)

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -8,9 +8,9 @@ class JobsController < ApplicationController
   end
 
   def show
-    @commentable = @job
-    @comments = @commentable.comments
-    @comment = Comment.new
+    #@commentable = @job
+    #@comments = @commentable.comments
+    #@comment = Comment.new
   end
 
   def new
