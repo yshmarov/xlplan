1d24a6fe784b616c185923726a4cf0a6e5c7c9dd
yashm <yshmarov@gmail.com>
2018-11-11 13:08:14 +0000

    calendar ux, stats - move some logic from view to controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,9 +7,19 @@ class StaticPagesController < ApplicationController
   end
 
   def stats
+    #company stats
+    @total_earnings = Job.all.map(&:client_price_cents).sum
+    @total_expences = Job.all.map(&:employee_price_cents).sum
+    @net_income = @total_earnings - @total_expences
+
+    #my_stats
+    @my_total_earnings = Job.where(employee_id: current_user.person.employee.id).map(&:employee_price_cents).sum
+
+    #Job statuses
     @job_statuses = Job.unscoped.group("status").count
-    next_bdays = (Date.today + 0.day).yday
+
     #next 5 bdays
+    next_bdays = (Date.today + 0.day).yday
     @people = Person.where("EXTRACT(DOY FROM date_of_birth) >= ?", next_bdays).order('EXTRACT (DOY FROM date_of_birth) ASC').first(5)
   end
 
