ba814ce4e26bac2efcb03fd30f06e87f7e953e87
yashm <yshmarov@gmail.com>
2018-11-17 20:13:24 +0000

    basic create employee_service_category in employee

--- app/controllers/employee_service_categories_controller.rb
+++ app/controllers/employee_service_categories_controller.rb
@@ -1,5 +1,5 @@
 class EmployeeServiceCategoriesController < ApplicationController
-  before_action :set_employee_service_category, only: [:show, :edit, :update, :destroy]
+  before_action :set_employee_service_category, only: [:show, :destroy]
 
   # GET /employee_service_categories
   # GET /employee_service_categories.json
@@ -17,18 +17,12 @@ class EmployeeServiceCategoriesController < ApplicationController
     @employee_service_category = EmployeeServiceCategory.new
   end
 
-  # GET /employee_service_categories/1/edit
-  def edit
-  end
-
-  # POST /employee_service_categories
-  # POST /employee_service_categories.json
   def create
     @employee_service_category = EmployeeServiceCategory.new(employee_service_category_params)
 
     respond_to do |format|
       if @employee_service_category.save
-        format.html { redirect_to @employee_service_category, notice: 'Employee service category was successfully created.' }
+        format.html { redirect_to employee_path(@employee_service_category.employee), notice: 'Employee service category was successfully created.' }
         format.json { render :show, status: :created, location: @employee_service_category }
       else
         format.html { render :new }
@@ -37,26 +31,10 @@ class EmployeeServiceCategoriesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /employee_service_categories/1
-  # PATCH/PUT /employee_service_categories/1.json
-  def update
-    respond_to do |format|
-      if @employee_service_category.update(employee_service_category_params)
-        format.html { redirect_to @employee_service_category, notice: 'Employee service category was successfully updated.' }
-        format.json { render :show, status: :ok, location: @employee_service_category }
-      else
-        format.html { render :edit }
-        format.json { render json: @employee_service_category.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /employee_service_categories/1
-  # DELETE /employee_service_categories/1.json
   def destroy
     @employee_service_category.destroy
     respond_to do |format|
-      format.html { redirect_to employee_service_categories_url, notice: 'Employee service category was successfully destroyed.' }
+      format.html { redirect_to employee_path(@employee_service_category.employee), notice: 'Employee service category was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
