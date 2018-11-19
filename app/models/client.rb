d490ceed1e0eda7a49fdc131684902ded28a824a
yashm <yshmarov@gmail.com>
2018-11-19 12:30:17 +0000

    better client/show and employee/show

--- app/models/client.rb
+++ app/models/client.rb
@@ -23,7 +23,7 @@ class Client < ApplicationRecord
   protected
   
   def update_balance
-    update_column :balance, (jobs.map(&:client_due_price).sum)
+    update_column :balance, (jobs.map(&:client_due_price).sum*(-1))
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable, :invitable, :validate_on_invite => true
 
   belongs_to :employee
+  belongs_to :invitor, class_name: 'Employee', foreign_key: :invited_by_id, required: false
 
   validates :employee_id, presence: true
   validates :employee_id, uniqueness: true
