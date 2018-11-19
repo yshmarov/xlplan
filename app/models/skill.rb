818f9b35304d3c314fff51d4d64e861028c6c4ee
yashm <yshmarov@gmail.com>
2018-11-19 23:06:23 +0000

    better forms ux

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -3,4 +3,5 @@ class Skill < ApplicationRecord
   belongs_to :service_category
   validates :employee_id, :service_category_id, presence: true
   validates_uniqueness_of :service_category_id, :scope => :employee_id
+
 end
