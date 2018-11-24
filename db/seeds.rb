feb09fc7377119935017f9c0264e4263590c0ef5
yashm <yshmarov@gmail.com>
2018-11-24 23:01:59 +0000

    disable public_activity for seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-User.create!(employee_id: 1, email:'yshmarov@gmail.com', password: 'yshmarov@gmail.com', confirmed_at: DateTime.now)
+PublicActivity.enabled = false
 Location.create!(name: 'Location-1', tel: 675760, balance: 0, status: 1)
-Workplace.create!(name: 'Workplace-1', location_id: 1)
 Employee.create!(first_name: 'Yaro', last_name: 'Shm', email: 'yshmarov@gmail.com', location_id: 1)
+User.create!(employee_id: 1, email:'yshmarov@gmail.com', password: 'yshmarov@gmail.com', confirmed_at: DateTime.now)
+
+#current_user = User.first
+
+Workplace.create!(name: 'Workplace-1', location_id: 1)
 Client.create!(first_name: 'Yuliia', last_name: 'Zapetaya', email: 'zapetaya.y@gmail.com', employee_id: 1)
 Client.create!(first_name: 'Iryna', last_name: 'Sharovarova', email: 'irina@gmail.com', employee_id: 1)
 Client.create!(first_name: 'Oleksiy', last_name: 'Seleznev', email: 'alex@gmail.com', employee_id: 1)
@@ -17,3 +21,4 @@ Job.create!(client_id: 1, employee_id: 1, location_id: 1, service_id: 1, created
 Job.create!(client_id: 1, employee_id: 1, location_id: 1, service_id: 2, created_by: 1, status: 1, starts_at: Time.now+1.hour)
 Job.create!(client_id: 2, employee_id: 1, location_id: 1, service_id: 3, created_by: 1, starts_at: Time.now+2.hours)
 Job.create!(client_id: 3, employee_id: 1, location_id: 1, service_id: 2, created_by: 1, starts_at: Time.now+2.hours)
+PublicActivity.enabled = true
