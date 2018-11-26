6f362de2c410d269bb9c839c8df7b77ab400d569
yashm <yshmarov@gmail.com>
2018-11-26 22:48:41 +0000

    public_activity for comments 01

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
-  #include PublicActivity::Model
-  #tracked owner: Proc.new{ |controller, model| controller.current_user }
+  include PublicActivity::Model
+  tracked owner: Proc.new{ |controller, model| controller.current_user }
 
   belongs_to :employee
   belongs_to :commentable, polymorphic: true, counter_cache: true
