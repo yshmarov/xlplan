aacc629102229dca95ef20021d4064afe768d0a4
yashm <yshmarov@gmail.com>
2018-12-08 22:22:34 +0000

    recaptcha 01

---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,6 @@
+Recaptcha.configure do |config|
+  config.site_key  = '6LecmH8UAAAAAO_6gLJSRFME1vHbcVRrK25zNGSv'
+  config.secret_key = '6LecmH8UAAAAAO_LxbZ3q_fyd5umfAO2Bo37m0CJ'
+  # Uncomment the following line if you are using a proxy server:
+  # config.proxy = 'http://myproxy.com.au:8080'
+end
