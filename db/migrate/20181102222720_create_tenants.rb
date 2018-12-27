e1d902444a9263c977fd829eaa3d36724c571003
yashm <yshmarov@gmail.com>
2018-12-27 23:44:16 +0000

    remove unnessesary migrations

--- db/migrate/20181102222720_create_tenants.rb
+++ db/migrate/20181102222720_create_tenants.rb
@@ -6,7 +6,7 @@ class CreateTenants < ActiveRecord::Migration[5.2]
       t.string :plan, :limit => 40, null: false
       t.string :industry, :limit => 60, null: false
       t.string :default_currency, :limit => 40, null: false, default: "usd"
-      #t.string :default_language, :limit => 40, null: false
+      t.string :locale, :string, default: 'en', null: false, :limit => 2
       #t.string :default_time_zone, null: false
       #t.string :country, null: false
       #t.string :city, null: false
--- db/migrate/20181227174858_add_locale_to_tenants.rb
+++
@@ -1,5 +0,0 @@
-class AddLocaleToTenants < ActiveRecord::Migration[5.2]
-  def change
-    add_column :tenants, :locale, :string, default: 'en', null: false
-  end
-end
