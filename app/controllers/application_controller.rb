b79638a1ebfc283dcd95966971b6e17dc16827fd
yashm <yshmarov@gmail.com>
2019-01-29 18:39:15 +0000

    app_controller - minor adjustments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,27 +5,21 @@ class ApplicationController < ActionController::Base
   ##    but you can override these if you wish to handle directly
   rescue_from ::Milia::Control::MaxTenantExceeded, :with => :max_tenants
   rescue_from ::Milia::Control::InvalidTenantAccess, :with => :invalid_tenant
-
   include Pundit
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-
-  before_action  :prep_org_name
-
-  #online?
-  after_action :user_activity
-
   include PublicActivity::StoreController 
 
-  #navbar search
+  before_action  :prep_org_name
+  after_action :user_activity
   before_action :set_global_search_variable
+  before_action :set_locale
+
+  private
+  #navbar search
   def set_global_search_variable
     @ransack_clients = Client.ransack(params[:clients_search], search_key: :clients_search)
   end
-
-  #i18n
-  before_action :set_locale
-  private
-
+  #milia
   def callback_authenticate_tenant
     @org_name = ( Tenant.current_tenant.nil?  ?
       "MrJobber"   :
@@ -33,34 +27,27 @@ class ApplicationController < ActionController::Base
     )
   end
   def prep_org_name()
-    @org_name ||= "MrJobber"
+    @org_name ||= "MrJobber - CRM for service business"
   end
-
   #i18n
   def set_locale
     if current_user
-      #I18n.locale = current_user.try(:locale) || I18n.default_locale
       I18n.locale = Tenant.current_tenant.try(:locale)  || I18n.default_locale
-      #I18n.locale = Tenant.current_tenant.locale
-      #I18n.locale = Tenant.current_tenant.locale.to_sym
-      #I18n.locale = current_user.member.tenant.locale.to_sym
-      #I18n.locale = current_user.member.tenant.locale.to_sym || I18n.default_locale
     else
-      #I18n.locale = params[:locale] || I18n.default_locale
       locale = params[:locale].to_s.strip.to_sym
       I18n.locale = I18n.available_locales.include?(locale) ?
           locale :
           I18n.default_locale
     end
   end
-
-    def user_not_authorized
-      flash[:alert] = "You are not authorized to access this page."
-      redirect_to(request.referrer || root_path)
-    end
-
-    def user_activity
-      current_user.try :touch
-    end
+  #pundit
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to access this page."
+    redirect_to(request.referrer || root_path)
+  end
+  #online?
+  def user_activity
+    current_user.try :touch
+  end
 
 end
