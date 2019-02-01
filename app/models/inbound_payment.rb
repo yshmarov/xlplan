031787c5801258d259c737efe9d12c5cfcecd5ae
yashm <yshmarov@gmail.com>
2019-02-01 09:29:51 +0000

    minor ux, payment PublicActivity

--- app/models/inbound_payment.rb
+++ app/models/inbound_payment.rb
@@ -13,6 +13,9 @@ class InboundPayment < ApplicationRecord
 
   PAYMENT_METHODS = [:cash, :credit_card, :bank_transfer, :subscription_card, :certificate, :barter, :gift, :free, :group_buying, :other] 
 
+  include PublicActivity::Model
+  tracked owner: Proc.new{ |controller, model| controller.current_user }
+
   def to_s
     id
   end
