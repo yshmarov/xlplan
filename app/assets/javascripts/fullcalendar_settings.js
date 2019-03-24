1c467ae2ee8e2a6a41da983a547a62248a993504
yashm <yshmarov@gmail.com>
2019-03-24 12:54:29 +0000

    better sidebar rendering

--- app/assets/javascripts/fullcalendar_settings.js
+++ app/assets/javascripts/fullcalendar_settings.js
@@ -19,7 +19,7 @@ function eventCalendar() {
     header: {
         left: 'prev,next, today',
         center: 'title',
-        right: 'agendaDay,agendaWeek,month,timelineDay'
+        right: 'agendaDay,agendaWeek'
     },
     titleFormat: 'D/MMM/YY',
     eventBackgroundColor: 'purple',
