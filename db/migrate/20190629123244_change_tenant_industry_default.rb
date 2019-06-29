60cf30344babcacb196bf574dfa6a9f5826f7cd3
yashm <yshmarov@gmail.com>
2019-06-29 13:12:00 +0000

    clean up migrations (tenant.industry)

--- db/migrate/20190629123244_change_tenant_industry_default.rb
+++
@@ -1,6 +0,0 @@
-class ChangeTenantIndustryDefault < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :tenants, :industry, :string
-    add_column :tenants, :industry, :string, limit: 144, default: "other", null: false
-  end
-end
