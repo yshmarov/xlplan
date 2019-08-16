8b1c5959ca5d7d011fbc9d277392dff4d7062ef9
YAROSLAV SHM <yshmarov@gmail.com>
2019-08-16 10:12:14 +0000

    omnicontacts saves contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,24 @@
 class ContactsController < ApplicationController
   def index
     @contacts = request.env['omnicontacts.contacts']
+
+    @contacts.each do |contact|
+      contact1 = Client.new
+      if contact[:first_name].present?
+        contact1.first_name = contact[:first_name]
+      else
+        contact1.first_name = contact[:name]
+      end
+      if contact[:last_name].present?
+        contact1.last_name = contact[:last_name]
+      else
+        contact1.last_name = contact[:name]
+      end
+      contact1.phone_number = contact[:phone_number]
+      contact1.email = contact[:email]
+      #contact1.date_of_birth = contact[:birthday]
+      contact1.save(:validate => false)
+    end
       respond_to do |format|
         format.html
       end
