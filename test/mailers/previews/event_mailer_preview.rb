71974a4c47d2013316c141dab087b3e3653c2ade
YAROSLAV SHM <yshmarov@gmail.com>
2019-08-19 19:18:18 +0000

    event_preview - find first client that has an email

--- test/mailers/previews/event_mailer_preview.rb
+++ test/mailers/previews/event_mailer_preview.rb
@@ -1,6 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/event_mailer
 class EventMailerPreview < ActionMailer::Preview
   def client_event_created
-    EventMailer.client_event_created(Event.last).deliver_now
+    #EventMailer.client_event_created(Event.last).deliver_now
+    #finds first event that has a client
+    EventMailer.client_event_created(Event.joins(:client).where.not(clients: {email: [nil, ""]}).first).deliver_now
   end
 end
