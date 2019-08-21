d3bdb26ad689df5769e3a884baf087ff5722ad05
YAROSLAV SHM <yshmarov@gmail.com>
2019-08-21 14:17:06 +0000

    expence/show.pdf.haml

--- app/controllers/expences_controller.rb
+++ app/controllers/expences_controller.rb
@@ -12,8 +12,9 @@ class ExpencesController < ApplicationController
         format.html
         format.pdf do
           render pdf: "Expence No. #{@expence.slug}",
-          page_size: 'A4',
+          page_size: 'A6',
           template: "expences/show.html.haml",
+          template: "expences/show.pdf.haml",
           layout: "pdf.haml",
           orientation: "Landscape",
           lowquality: true,
