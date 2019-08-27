8797b8b8fd63dafb4bc40c71a91d454725d08523
YAROSLAV SHM <yshmarov@gmail.com>
2019-08-27 15:07:31 +0000

    clean up migrations

--- db/migrate/20190826185414_add_instasignupdetails_to_tenants.rb
+++
@@ -1,6 +0,0 @@
-class AddInstasignupdetailsToTenants < ActiveRecord::Migration[5.2]
-  def change
-    add_column :tenants, :phone, :string
-    add_column :tenants, :coupon, :string
-  end
-end
--- db/migrate/20190826191436_remove_instasginupdetails_from_tenants.rb
+++
@@ -1,6 +0,0 @@
-class RemoveInstasginupdetailsFromTenants < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :tenants, :coupon
-    remove_column :tenants, :phone
-  end
-end
