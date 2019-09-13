cb36cf9a83bfec6e4b1fb64fecbe4fb4cb247747
EC2 Default User <yashm@outlook.com>
2019-09-13 20:36:29 +0000

    landing_page - better UX

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  #config.action_mailer.default_url_options = { :host => 'bf020d3ad8e840fe89766c5a2d1fcc54.vfs.cloud9.us-east-1.amazonaws.com', :protocol => 'https' }
-  config.action_mailer.default_url_options = { :host => 'https://bf020d3ad8e840fe89766c5a2d1fcc54.vfs.cloud9.us-east-1.amazonaws.com'}
+  #config.action_mailer.default_url_options = { :host => 'd4db634c21ab4b44baaef6d890ed8d63.vfs.cloud9.us-east-1.amazonaws.com', :protocol => 'https' }
+  config.action_mailer.default_url_options = { :host => 'https://d4db634c21ab4b44baaef6d890ed8d63.vfs.cloud9.us-east-1.amazonaws.com'}
 
   #config.action_mailer.delivery_method = :smtp
   config.action_mailer.delivery_method = :test
