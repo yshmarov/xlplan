79b7172ec8081dcfc60c891221da0dd629d74480
Yaro <yshmarov@gmail.com>
2019-10-05 23:33:02 +0000

    payment_method i18n

--- app/models/expence.rb
+++ app/models/expence.rb
@@ -1,22 +1,28 @@
 class Expence < ApplicationRecord
+  #-----------------------gem milia-------------------#
   acts_as_tenant
-
+  #-----------------------relationships-------------------#
   belongs_to :tenant
   belongs_to :expendable, polymorphic: true, touch: true, counter_cache: true
-  
+
+  #-----------------------validation-------------------#
+  #validates :client, :amount, :amount_cents, :payment_method, presence: true
   validates :amount, :payment_method, presence: true
   validates :amount, :numericality => {:greater_than => -10000000000, :less_than => 10000000000}
   validates :slug, uniqueness: true
   validates :slug, uniqueness: { case_sensitive: false }
-
+  #-----------------------gem money-------------------#
   monetize :amount, as: :amount_cents
-  #validates :client, :amount, :amount_cents, :payment_method, presence: true
-
-  PAYMENT_METHODS = [:cash, :bank_transfer]
-
+  #-----------------------payment_method options--------------------------------#
+  PAYMENT_METHODS = [:cash, :credit_card, :bank_transfer] 
+  def self.payment_methods
+    PAYMENT_METHODS.map {|payment_method| [I18n.t(payment_method, scope: [:activerecord, :attributes, :inbound_payment, :payment_methods]).capitalize, payment_method]}
+  end
+  #-----------------------gem public_activity-------------------#
   include PublicActivity::Model
   tracked owner: Proc.new{ |controller, model| controller.current_user }
   tracked tenant_id: Proc.new{ Tenant.current_tenant.id }
+  #-----------------------gem friendly_id-------------------#
   extend FriendlyId
   friendly_id :generated_slug, use: :slugged
   def generated_slug
--- app/models/inbound_payment.rb
+++ app/models/inbound_payment.rb
@@ -1,23 +1,28 @@
 class InboundPayment < ApplicationRecord
+  #-----------------------gem milia-------------------#
   acts_as_tenant
-
+  #-----------------------relationships-------------------#
   belongs_to :tenant
   belongs_to :client, touch: true, counter_cache: true
   belongs_to :payable, polymorphic: true
-
-  validates :amount, :payment_method, presence: true
+  #-----------------------validation-------------------#
+  #validates :amount, :payment_method, presence: true
+  validates :client, :amount, :amount_cents, :payment_method, presence: true
   validates :amount, :numericality => {:greater_than => -10000000000, :less_than => 10000000000}
   validates :slug, uniqueness: true
   validates :slug, uniqueness: { case_sensitive: false }
-
+  #-----------------------gem money-------------------#
   monetize :amount, as: :amount_cents
-  validates :client, :amount, :amount_cents, :payment_method, presence: true
-
-  PAYMENT_METHODS = [:cash, :bank_transfer, :credit_card] 
-
+  #-----------------------payment_method options--------------------------------#
+  PAYMENT_METHODS = [:cash, :credit_card, :bank_transfer] 
+  def self.payment_methods
+    PAYMENT_METHODS.map {|payment_method| [I18n.t(payment_method, scope: [:activerecord, :attributes, :inbound_payment, :payment_methods]).capitalize, payment_method]}
+  end
+  #-----------------------gem public_activity-------------------#
   include PublicActivity::Model
   tracked owner: Proc.new{ |controller, model| controller.current_user }
   tracked tenant_id: Proc.new{ Tenant.current_tenant.id }
+  #-----------------------gem friendly_id-------------------#
   extend FriendlyId
   friendly_id :generated_slug, use: :slugged
   def generated_slug
