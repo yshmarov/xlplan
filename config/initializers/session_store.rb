698c7b99facb2991770e9d9806ca54651b329420
Yaro <yshmarov@gmail.com>
2019-10-30 08:11:11 +0000

    remove some autofocuses (looks not cool in mobile view), booking/show - ux

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,2 @@
 #Rails.application.config.session_store :active_record_store, :key => '_xlplan_session'
+#Rails.application.config.session_store :cookie_store, key: '_app_session', expire_after: 3.hours
