1f5be8ff146bc2a831d43903ca5203178017cbfc
Yaro <yshmarov@gmail.com>
2019-11-11 01:43:09 +0000

    current_user can access static_pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :authenticate_tenant!, :only => [ :landing_page, :features, :pricing, :privacy_policy, :terms_of_service, :security, :stats]
+  skip_before_action :authenticate_tenant!, :only => [:landing_page, :features, :pricing, :privacy_policy, :terms_of_service, :security, :stats]
 
   def landing_page
     if current_user
@@ -8,33 +8,18 @@ class StaticPagesController < ApplicationController
   end
 
   def features
-    if current_user
-      redirect_to calendar_path
-    end
   end
   
   def pricing
-    if current_user
-      redirect_to calendar_path
-    end
   end
 
   def privacy_policy
-    if current_user
-      redirect_to calendar_path
-    end
   end
   
   def terms_of_service
-    if current_user
-      redirect_to calendar_path
-    end
   end
   
   def security
-    if current_user
-      redirect_to calendar_path
-    end
   end
 
   before_action :authenticate, only: :stats
