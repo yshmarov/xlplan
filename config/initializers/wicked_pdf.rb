91807e262360d7a265b631555854a0189ba8b210
yashm <yashm@outlook.com>
2020-01-20 09:13:44 +0000

    wicked_pdf was not working in production. try to make it work 02

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -13,7 +13,7 @@ WickedPdf.config = {
   # one of the wkhtmltopdf-binary family of gems.
   #exe_path: '/usr/local/bin/wkhtmltopdf',
   #   or
-  exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf', 'wkhtmltopdf-heroku')
+  exe_path: Gem.bin_path('wkhtmltopdf-heroku')
   #exe_path: Rails.root.join('bin', 'wkhtmltopdf').to_s
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
