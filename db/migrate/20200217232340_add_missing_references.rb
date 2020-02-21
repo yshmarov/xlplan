16df2472d2703cd0504701dc14b455b0e104128a
Yaro <yshmarov@gmail.com>
2020-02-21 23:44:46 +0000

    update migrations

--- db/migrate/20200217232340_add_missing_references.rb
+++
@@ -1,6 +0,0 @@
-class AddMissingReferences < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :workplaces, :events, index: true
-    add_reference :cash_accounts, :transactions, index: true
-  end
-end
--- db/migrate/20200217232350_add_missing_references_updated.rb
+++ db/migrate/20200217232350_add_missing_references_updated.rb
@@ -1,8 +1,5 @@
 class AddMissingReferencesUpdated < ActiveRecord::Migration[5.2]
   def change
-    remove_reference :workplaces, :events, index: true
-    remove_reference :cash_accounts, :transactions, index: true
-
     add_reference :events, :workplaces, index: true
     add_reference :transactions, :cash_accounts, index: true
   end
