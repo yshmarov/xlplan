7b7380a0e7b8683b5dea7e05a0624132f7338281
Yaro <yshmarov@gmail.com>
2020-03-24 18:18:20 +0000

    clean up migrations

--- db/migrate/20200324163717_remove_some_non_related_member_fields.rb
+++
@@ -1,9 +0,0 @@
-class RemoveSomeNonRelatedMemberFields < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :members, :gender
-    remove_column :members, :country
-    remove_column :members, :city
-    remove_column :members, :zip
-    remove_column :members, :address
-  end
-end
--- db/migrate/20200324165556_remote_dob_from_members.rb
+++
@@ -1,5 +0,0 @@
-class RemoteDobFromMembers < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :members, :date_of_birth
-  end
-end
