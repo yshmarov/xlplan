2f6580ee2d93fae803e4fee41731ec8b9cbaad8f
Yaro <yshmarov@gmail.com>
2020-03-29 18:19:37 +0000

    work on speeding up the app with global variables

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,11 +14,10 @@ class ApplicationController < ActionController::Base
   include PublicActivity::StoreController #save current_user using gem public_activity
 
   before_action :set_locale
-  before_action :set_global_search_variable, if: :user_signed_in?
+  before_action :set_global_variables, if: :user_signed_in?
   #after_action :user_activity, if: :user_signed_in?
   around_action :set_time_zone, if: :user_signed_in?
-
-  before_action  :prep_org_name
+  before_action :callback_authenticate_tenant
 
   private
     #i18n
@@ -56,7 +55,8 @@ class ApplicationController < ActionController::Base
     end
   
     #navbar search
-    def set_global_search_variable
+    def set_global_variables
+      @tenant = Tenant.current_tenant
       @ransack_clients = Client.ransack(params[:clients_search], search_key: :clients_search)
       @event_close_count = Event.close.count
       @client_debtors_count = Client.debtors.count
@@ -69,17 +69,13 @@ class ApplicationController < ActionController::Base
     #end
   
     #GEM MILIA
-    # optional callback for post-authenticate_tenant! processing
     def callback_authenticate_tenant
       @org_name = ( Tenant.current_tenant.nil?  ?
         "XLPLAN"   :
         Tenant.current_tenant.name 
       )
-      # set_environment or whatever else you need for each valid session
     end
   
-    #   org_name will be passed to layout & view
-    #   this sets the default name for all situations
     def prep_org_name()
       @org_name ||= "CRM for service business management"
     end
